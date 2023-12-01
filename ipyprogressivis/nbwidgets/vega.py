6dc64546cefc600439095b142c18c9cbc70a450f
Christian Poli <Christian.Poli@inria.fr>
2023-12-01 16:52:25 +0100

    fixes

--- ipyprogressivis/nbwidgets/vega.py
+++ ipyprogressivis/nbwidgets/vega.py
@@ -1,7 +1,7 @@
 import ipywidgets
 from traitlets import Unicode
 
-
+from typing import Any
 import vega.widget as vw
 
 @ipywidgets.register
@@ -11,5 +11,5 @@ class VegaWidget(vw.VegaWidget):
     _view_module_version = Unicode("^0.1.0").tag(sync=True)
     _model_module_version = Unicode("^0.1.0").tag(sync=True)
 
-    def __init__(self, *args, **kw):
+    def __init__(self, *args: Any, **kw: Any) -> None:
         super().__init__(*args, **kw)
