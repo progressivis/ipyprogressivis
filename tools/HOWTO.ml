0ab45dad8f7f04d889fa25cb2351f8d31d74a0db
Christian Poli <Christian.Poli@inria.fr>
2025-02-04 14:45:04 +0100

    nb to html conversion tool[skip ci]

--- tools/HOWTO.ml
+++ tools/HOWTO.ml
@@ -1,3 +1,7 @@
+# Documentation tools HOWTO
+
+## Merging ProgressiBooks and markdown files (`merge_nb_markdown.py` script)
+
 The `merge_nb_markdown.py` script merges a `ProgressiBook` with a markdown file containing exactly the same titles.
 
 In other words, you can edit and manage the documentation of a `ProgressiBook` in a separate file and reinject it into the same `ProgressiBook` (or a new one) when you need to redo it from scratch.
@@ -30,4 +34,28 @@ Options:
                             homonym
   --help                    Show this message and exit.
 
+```
+
+## Generating HTML files from ProgressiBooks (`nb_to_doc_html.py` script)
+
+ProgressiBooks cannot be converted to clean html via `jupyter nbconvert`command due to some technical peculiarities (mainly hidden cells and use of the Sidecar widget). The following script overcomes these problems and produces self-sufficient html files that can be easily integrated into Sphinx documentation. As the output is a directory, the file generated will have the same name as the `ProgressiBook`, but with the extension changed to 'html':
+
+Translated with DeepL.com (free version)
+
+```sh
+$ python tools/nb_to_doc_html.py -p notebooks/userguide-widgets1.1.ipynb -o notebooks/
+```
+
+A help is also available:
+
+```sh
+$ python tools/nb_to_doc_html.py --help
+Usage: nb_to_doc_html.py [OPTIONS]
+
+Options:
+  -p, --progressibook TEXT  Input ProgressiBook
+  -w, --overwrite           Overwrite HTML file if exists
+  -o, --output TEXT         Destination HTML. If it is a directory the
+                            ProgressiBook name is used with extension '.html'
+  --help                    Show this message and exit.
 ```
---
+++ tools/nb_to_doc_html.py
@@ -0,0 +1,60 @@
+import sys
+import json
+import subprocess
+import shlex
+import shutil
+import click
+from pathlib import Path
+from tempfile import TemporaryDirectory
+import os
+
+
+@click.command()
+@click.option("-p", "--progressibook", help="Input ProgressiBook")
+@click.option("-w", "--overwrite", is_flag=True, help="Overwrite HTML file if exists")
+@click.option("-o", "--output", help=("Destination HTML. If it is a directory"
+                                                  " the ProgressiBook name is used with extension '.html'"))
+
+
+def main(progressibook: str, overwrite: bool, output: str) -> None:
+    print(progressibook, output)
+    pb_name = os.path.basename(progressibook)
+    bare_name, extn = pb_name.rsplit(".", 1)
+    assert extn == "ipynb"
+    html_name = f"{bare_name}.html"
+    if os.path.isdir(output):
+        out_name  = Path(output) / html_name
+    else:
+        out_name  = Path(output)
+    if os.path.exists(out_name) and not overwrite:
+        print(f"{out_name=} already exists. Use --overwrite to replace it")
+        sys.exit(1)
+    with open(progressibook) as f:
+        pb = json.load(f)
+    cells = pb.get("cells", [])
+    if len(cells) < 4 or not cells[0].get("metadata", {}).get("progressivis_backup"):
+        print("probably not a ProgressiBook")
+        sys.exit(1)
+    cell_1 = cells[1]
+    cell_1["source"] = ["..."]
+    if dag_png := pb.get("metadata", {}).get("progressivis_dag_png"):
+        prefix, b64_data = dag_png.split(",", 1)
+        cell_1["outputs"] = [{"data": {"image/png": b64_data},
+                              "output_type": "display_data", "metadata": {}}]
+    else:
+        cell_1["outputs"] = []
+    root_cell = cells[3]
+    root_cell["source"] = ["..."]
+    root_cell["outputs"] = []
+    with TemporaryDirectory() as tmpdirname:
+        tmpdir = Path(tmpdirname)
+        nbdoc_path = tmpdir / pb_name
+        with open(nbdoc_path, "w") as f:
+            json.dump(pb, f)
+        cmd = f"jupyter nbconvert --to html --template lab {nbdoc_path}"
+        subprocess.run(shlex.split(cmd))
+        shutil.copy(tmpdir / html_name, out_name)
+
+if __name__ == "__main__":
+    main()
+
