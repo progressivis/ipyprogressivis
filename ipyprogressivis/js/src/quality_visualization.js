5e002956d094197c91bd601be09dcb4f7faec08d
FEKETE Jean-Daniel <jean-daniel.fekete@inria.fr>
2025-09-07 21:24:26 +0200

    Improve quality: use log scale for time, avoid too crowded points

--- ipyprogressivis/js/src/quality_visualization.js
+++ ipyprogressivis/js/src/quality_visualization.js
@@ -80,7 +80,8 @@ export class QualityVisualizationView extends widgets.DOMWidgetView {
 
 function quality_pbar(parent, w, h) {
   let n = 0,
-      margin = 5,
+      topMargin = 5,
+      bottomMargin = 15,
       width = w,
       height = h,
       svg = d3.create("svg")
@@ -88,8 +89,9 @@ function quality_pbar(parent, w, h) {
         .attr("width", width)
         .attr("height", height),
       g = svg.append("g"),
+      gx = svg.append("g")
+        .attr("transform", `translate(0, ${height-bottomMargin})`),
       all_measures = {},
-      all_polylines = {},
       all_scales = {},
       min_ts = Number.POSITIVE_INFINITY,
       max_ts = Number.NEGATIVE_INFINITY,
@@ -112,38 +114,46 @@ function quality_pbar(parent, w, h) {
     return arguments.length ? decimate_threshold = +_ : decimate_threshold;
   }
 
-  function round3(x) {
-    return Math.round(x * 1000) / 1000;
-  }
+  // function round3(x) {
+  //   return Math.round(x * 1000) / 1000;
+  // }
 
-  function decimate() {
-    for (const measure in all_measures) {
-      if (all_measures[measure].length < decimate_threshold) continue;
-      const half = Math.floor(all_measures[measure].length / 2);
-      for (let i = half; i > 0; i -= 2) { // always keep index 0
-        all_measures[measure].splice(i, 1);
-      }
-      all_polylines[measure] = "";
-      for (const [ts, val] of all_measures[measure]) {
-        all_polylines[measure] += ` ${ts}, ${round3(val)}`;
-      }
-    }
-  }
+  // function decimate() {
+  //   for (const measure in all_measures) {
+  //     if (all_measures[measure].length < decimate_threshold) continue;
+  //     const half = Math.floor(all_measures[measure].length / 2);
+  //     for (let i = half; i > 0; i -= 2) { // always keep index 0
+  //       all_measures[measure].splice(i, 1);
+  //     }
+  //     all_polylines[measure] = "";
+  //     for (const [ts, val] of all_measures[measure]) {
+  //       all_polylines[measure] += ` ${round3(ts)}, ${round3(val)}`;
+  //     }
+  //   }
+  // }
 
   function add(ts, measures) {
+    const old_max = max_ts,
+          old_min = min_ts;
     max_ts = Math.max(max_ts, ts);
     min_ts = Math.min(min_ts, ts);
-    const xwidth = max_ts - min_ts,
-          xscale = xwidth == 0 ? 1 : (width - 2*margin) / xwidth;
-    g.attr("transform",
-           `translate(${margin}, ${-margin}) scale(${round3(xscale)}, -1) translate(${round3(-min_ts)}, ${-height})`);
+    const x = d3.scalePow([Math.ceil(min_ts), Math.floor(max_ts)],
+                          [topMargin, width-2*topMargin]).exponent(2);
+    if (old_max != max_ts || old_min != min_ts) {
+      gx.call(d3.axisBottom(x));
+      gx.attr("font-size", "8")
+        .classed("qaxis");
+    }
+    //const xwidth = max_ts - min_ts,
+    //      xscale = xwidth == 0 ? 1 : (width - 2*margin) / xwidth;
+    //g.attr("transform",
+    //       `translate(${margin}, ${-margin}) scale(${round3(xscale)}, -1) translate(${round3(-min_ts)}, ${-height})`);
 
     for (const [measure, val] of Object.entries(measures)) {
       var scale;
       if (all_measures[measure] === undefined) {
         const scheme = d3.schemeCategory10;
         all_measures[measure] = [];
-        all_polylines[measure] = "";
         scale = {xmin: ts, xmax: ts, ymin: val, ymax: val};
         all_scales[measure] = scale;
         all_elements[measure] = g.append("polyline")
@@ -166,18 +176,30 @@ function quality_pbar(parent, w, h) {
         scale.ymin = Math.min(scale.ymin, val);
         scale.ymax = Math.max(scale.ymax, val);
       }
-      const ymin = scale.ymin,
-            yheight = scale.ymax - ymin,
-            yscale = yheight === 0 ? 1 : (height - 2*margin) / yheight;
+      // const ymin = scale.ymin,
+      //       yheight = scale.ymax - ymin,
+      //       yscale = yheight === 0 ? 1 : (height - 2*margin) / yheight;
+      const y = d3.scaleLinear([scale.ymin, scale.ymax],
+                               [height-topMargin-bottomMargin, topMargin]);
       // decimate();
       all_measures[measure].push([ts, val]);
-      all_polylines[measure] += ` ${round3(ts)}, ${round3(val)}`;
-      all_elements[measure]
-        .attr(
-          "transform",
-          `scale(1, ${round3(yscale)}) translate(0, ${round3(-ymin)})`
-        )
-        .attr("points", all_polylines[measure]);
+      let polyline = "",
+          prev_x;
+      for (const [time, value] of all_measures[measure]) {
+        const cur_x = x(time),
+              cur_y = y(value);
+        if (prev_x === undefined || (cur_x - prev_x) > 2) {
+          polyline += ` ${cur_x}, ${cur_y}`;
+          prev_x = cur_x;
+          // Maybe discard the value in all_measures[measure] in the future
+        }
+      }
+      all_elements[measure].attr("points", polyline);
+        // .attr(
+        //   "transform",
+        //   `scale(1, ${round3(yscale)}) translate(0, ${round3(-ymin)})`
+        // )
+        
     }
   }
   quality_pbar.add = add;
