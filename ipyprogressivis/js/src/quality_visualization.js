83d51c91c000c25a4aa695aa2c71f5d926c21b8b
FEKETE Jean-Daniel <jean-daniel.fekete@inria.fr>
2025-09-08 10:58:28 +0200

    Improve quality visualization, use time in x axis, remove old points too close, use a fixed precision in polylines.

--- ipyprogressivis/js/src/quality_visualization.js
+++ ipyprogressivis/js/src/quality_visualization.js
@@ -78,13 +78,27 @@ export class QualityVisualizationView extends widgets.DOMWidgetView {
   }
 }
 
+function _format_seconds(ts, formater) {
+  return formater.format({
+    days: Math.floor(ts / (60*60*24)),
+    hours: Math.floor(ts / (60*60)) % 24,
+    minutes: Math.floor(ts / 60) % 60,
+    seconds: Math.floor(ts) % 60
+  });
+}
+
 function quality_pbar(parent, w, h) {
   let n = 0,
       topMargin = 5,
-      bottomMargin = 15,
+      bottomMargin = 20,
+      leftMargin = 5,
+      rightMargin = 5,
       width = w,
       height = h,
-      svg = d3.create("svg")
+      decimate_threshold = 2, // min distance between two visible points
+      min_ts = Number.POSITIVE_INFINITY,
+      max_ts = Number.NEGATIVE_INFINITY;
+  const svg = d3.create("svg")
         .classed("quality-vis", true)
         .attr("width", width)
         .attr("height", height),
@@ -93,10 +107,9 @@ function quality_pbar(parent, w, h) {
         .attr("transform", `translate(0, ${height-bottomMargin})`),
       all_measures = {},
       all_scales = {},
-      min_ts = Number.POSITIVE_INFINITY,
-      max_ts = Number.NEGATIVE_INFINITY,
       all_elements = {},
-      decimate_threshold = width;
+      durationFormat = new Intl.DurationFormat("en", {style: "digital"}),
+      seconds_formatter = ((ts) => _format_seconds(ts - min_ts, durationFormat));
 
   parent.append(svg.node());
 
@@ -118,36 +131,22 @@ function quality_pbar(parent, w, h) {
   //   return Math.round(x * 1000) / 1000;
   // }
 
-  // function decimate() {
-  //   for (const measure in all_measures) {
-  //     if (all_measures[measure].length < decimate_threshold) continue;
-  //     const half = Math.floor(all_measures[measure].length / 2);
-  //     for (let i = half; i > 0; i -= 2) { // always keep index 0
-  //       all_measures[measure].splice(i, 1);
-  //     }
-  //     all_polylines[measure] = "";
-  //     for (const [ts, val] of all_measures[measure]) {
-  //       all_polylines[measure] += ` ${round3(ts)}, ${round3(val)}`;
-  //     }
-  //   }
-  // }
-
   function add(ts, measures) {
     const old_max = max_ts,
           old_min = min_ts;
     max_ts = Math.max(max_ts, ts);
     min_ts = Math.min(min_ts, ts);
     const x = d3.scalePow([Math.ceil(min_ts), Math.floor(max_ts)],
-                          [topMargin, width-2*topMargin]).exponent(2);
+                          [leftMargin, width-leftMargin-rightMargin])
+          .exponent(2);
     if (old_max != max_ts || old_min != min_ts) {
-      gx.call(d3.axisBottom(x));
-      gx.attr("font-size", "8")
-        .classed("qaxis");
+      const axis = d3.axisBottom(x)
+            .ticks(3)
+            .tickFormat(seconds_formatter)
+            .tickSizeInner(4);
+      gx.call(axis);
+      gx.classed("qaxis");
     }
-    //const xwidth = max_ts - min_ts,
-    //      xscale = xwidth == 0 ? 1 : (width - 2*margin) / xwidth;
-    //g.attr("transform",
-    //       `translate(${margin}, ${-margin}) scale(${round3(xscale)}, -1) translate(${round3(-min_ts)}, ${-height})`);
 
     for (const [measure, val] of Object.entries(measures)) {
       var scale;
@@ -176,30 +175,26 @@ function quality_pbar(parent, w, h) {
         scale.ymin = Math.min(scale.ymin, val);
         scale.ymax = Math.max(scale.ymax, val);
       }
-      // const ymin = scale.ymin,
-      //       yheight = scale.ymax - ymin,
-      //       yscale = yheight === 0 ? 1 : (height - 2*margin) / yheight;
       const y = d3.scaleLinear([scale.ymin, scale.ymax],
                                [height-topMargin-bottomMargin, topMargin]);
-      // decimate();
       all_measures[measure].push([ts, val]);
+      const measures = all_measures[measure];
       let polyline = "",
           prev_x;
-      for (const [time, value] of all_measures[measure]) {
+      for (let i = 0; i < measures.length; ) {
+        const [time, value] = measures[i];
         const cur_x = x(time),
               cur_y = y(value);
-        if (prev_x === undefined || (cur_x - prev_x) > 2) {
-          polyline += ` ${cur_x}, ${cur_y}`;
+        if (prev_x === undefined || (cur_x - prev_x) > decimate_threshold) {
+          polyline += ` ${cur_x.toPrecision(4)}, ${cur_y.toPrecision(4)}`;
           prev_x = cur_x;
-          // Maybe discard the value in all_measures[measure] in the future
+          i++;
+        }
+        else {
+          measures.splice(i, 1); // remove point too close to previous
         }
       }
       all_elements[measure].attr("points", polyline);
-        // .attr(
-        //   "transform",
-        //   `scale(1, ${round3(yscale)}) translate(0, ${round3(-ymin)})`
-        // )
-        
     }
   }
   quality_pbar.add = add;
